3ce4ada2b6a3dc42e714fecaea86f79609a32392
Ayomide <oyetimehin31@gmail.com>
2025-10-15 20:35:07 +0100

    refactor: handle potential api failuer and set appropriate timeout

--- src/main/java/com/hng/DynamicProfileEndpoint/service/FetchCatFactService.java
+++ src/main/java/com/hng/DynamicProfileEndpoint/service/FetchCatFactService.java
@@ -1,6 +1,8 @@
 package com.hng.DynamicProfileEndpoint.service;
 
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
@@ -8,11 +10,29 @@ import java.util.Map;
 @Service
 public class FetchCatFactService {
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
+    String CAT_FACT_URL = "https://catfact.ninja/fact";
+
+    public FetchCatFactService() {
+//        Timeout Configuration
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(4000);
+        factory.setReadTimeout(4000);
+
+        this.restTemplate = new RestTemplate(factory);
+    }
 
     public String getRandomCatFact() {
-        String CAT_FACT_URL = "https://catfact.ninja/fact";
-        Map<String, Object> response = restTemplate.getForObject(CAT_FACT_URL, Map.class);
-        return response != null ? (String) response.get("fact") : "No fact available. Try again!";
+        try {
+            Map<String, Object> response = restTemplate.getForObject(CAT_FACT_URL, Map.class);
+            if (response != null && response.containsKey("fact")) {
+                return (String) response.get("fact");
+            } else {
+                return "No cat fact is available at the moment!";
+            }
+        } catch (RestClientException e) {
+            System.err.println("Error fetching cat fact: " + e.getMessage());
+            return "Could not fetch cat fact. Please try again later!";
+        }
     }
 }
